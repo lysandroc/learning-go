3308411615dae8fa4614f49f7e0142e5d0ab7887
Lysandro Carioca <lysandroc@gmail.com>
2020-04-14 21:11:50 -0300

    basics of struct

---
+++ go-struct.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	type car struct {
+		weight    float64
+		sizeWheel int
+		color     string
+	}
+
+	var carX car
+	carX.color = "blue"
+	carX.weight = 6.250
+	carX.sizeWheel = 17
+	fmt.Println(carX)
+
+	CarY := car{
+		color:     "red",
+		sizeWheel: 20,
+		weight:    7.920,
+	}
+	fmt.Println(CarY)
+
+	//this create a new pointer
+	CarZ := new(car)
+	CarZ.color = "red"
+	CarZ.sizeWheel = 16
+	CarZ.weight = 5.500
+	fmt.Println(CarZ)
+}
