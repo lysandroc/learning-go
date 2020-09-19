f2ef1a35fca95c7a97bf2d41d13d0227d0291240
Lysandro Carioca <lysandroc@gmail.com>
2020-09-18 23:01:04 -0300

    teste

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 	f.DeferGotchaImproved()
 	f.DeferGotchaImprovedAnonymousFunction()
 	//Learning variadic function
-	nums := []int{1, 2, 3}
+	nums := []int{1, 2, 3, 4}
 	f.VariadicFunction(nums...)
 }
